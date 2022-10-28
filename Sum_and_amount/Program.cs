aef840e12139d440f3958efee477ae3255f50ddb
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-28 23:12:35 +0400

    test11

---
+++ Sum_and_amount/Program.cs
@@ -0,0 +1,30 @@
+﻿int sumDivision = 0;
+int amountDivision = 0;
+int count;
+int limit;
+int variable;
+Console.WriteLine("Enter first num: " );
+count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter last num: " );
+limit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the variable you want to divide the number by : " );
+variable = Convert.ToInt32(Console.ReadLine());
+
+while (count <= limit)
+{
+    if (count % variable == 0)
+    {
+        sumDivision = sumDivision + count;
+        amountDivision++;
+    }
+     
+    
+count++;
+}
+
+
+Console.WriteLine("Sum of numbers which divide on variable: " + sumDivision);
+Console.WriteLine("Amount of numbers which divide on variable: " + amountDivision);
+
+
+
